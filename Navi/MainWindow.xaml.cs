9ef287c113b0d29381e1b79bffb4d687fbd14cb1
Jeme <eemelijoonatan@gmail.com>
2020-06-03 19:49:04 +0300

    Project start: functionality started.

--- Navi/MainWindow.xaml.cs
+++ Navi/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+
 
 namespace Navi
 {
@@ -20,9 +16,89 @@ namespace Navi
     /// </summary>
     public partial class MainWindow : Window
     {
+        /**
+        private YoutubeClient youtube;
+        private YoutubeConverter youtubeConverter;
+        private MediaPlayer snd;        
+
+        private bool isPlayingAudio;
+
+        private readonly string youtubeID = "https://www.youtube.com/watch?v=_JL33DgClrI&list=RD_JL33DgClrI&start_radio=1";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            isPlayingAudio = false;
+
+            youtube = new YoutubeClient();
+            youtubeConverter = new YoutubeConverter(youtube);
         }
+
+        private void checkLibraryStatus()
+        {
+            if (!Directory.Exists("./library"))
+            {
+                Directory.CreateDirectory("./library");
+            }
+        }
+
+        private async void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //if (txtBox.Text == "") return; TODO: COMMENT THIS OUT
+
+            try
+            {
+                var video = await youtube.Videos.GetAsync(youtubeID);
+                var title = cleanTitle(video.Title);
+                labelResult.Content = title;
+                imgThumbnail.Source = new BitmapImage(new Uri(video.Thumbnails.StandardResUrl));
+
+                checkLibraryStatus();
+
+                var destinationPath = Path.Combine("./library/test1/", $"{title}.mp3");
+                downloadImageAndAudio(youtubeID, destinationPath, video);
+
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("The song is already in the library", "Error");
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("SOMETHING HAPPANED", "Error"); //TODO: FIX ERRORGHANDLING
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Invalid Youtube video id or url.", er.ToString());
+            }
+        }
+
+        private string cleanTitle(string title)
+        {
+            return string.Join("_", title.Split(Path.GetInvalidFileNameChars()));
+        }
+
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isPlayingAudio)
+            {
+                snd = new MediaPlayer();
+                snd.Volume = 0.05f;
+                snd.Open(new Uri(Environment.CurrentDirectory + "/library/test1/Hanatan - Ghost Rule.mp3"));
+                snd.Play();
+                isPlayingAudio = true;
+            }
+        }
+
+
+        private async void downloadImageAndAudio(string youtubeID, string destinationPath, YoutubeExplode.Videos.Video video)
+        {
+            using (WebClient webClient = new WebClient())
+            { 
+                webClient.DownloadFileAsync(new Uri(video.Thumbnails.StandardResUrl), $"./library/test1/{video.Title}.png");
+            }
+            await youtubeConverter.DownloadVideoAsync(youtubeID, destinationPath);
+        }**/
     }
 }
